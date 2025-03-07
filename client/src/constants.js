9eba9a59e0da8ce66358d0808cb6af5dd413b851
Kartikey Sahu <117040305+kartikeysahu987@users.noreply.github.com>
2025-03-07 14:26:51 +0530

    Update constants.js

--- client/src/constants.js
+++ client/src/constants.js
@@ -1,2 +1,2 @@
 // export const BASE_URL = "http://172.30.8.212:4751";
-export const BASE_URL = "http://localhost:4751";
+export const BASE_URL = "https://kartibackendtemp-production.up.railway.app/";
